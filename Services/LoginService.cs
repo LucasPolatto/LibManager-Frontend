99be31dccc1691030d966a49607a6fe06356ecbd
Lucas Polatto <67125032+LucasPolatto@users.noreply.github.com>
2022-11-23 11:27:33 -0300

    Telas principais

--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -1,5 +1,14 @@
 public class LoginService
 {
-    public bool Logado { get; set; } = false;
+    public int? Id { get; set; }
     public string? User { get; set; }
+    public string? Password { get; set; }
+    public bool Logado { get; set; } = false;
+
+    public void LogOut()
+    {
+        Id = null;
+        User = null;
+        Logado = false;
+    }
 }
