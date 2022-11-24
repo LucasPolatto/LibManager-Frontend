e0e704e2013db47c97fd66f873dd39fe59c487ee
Lucas Polatto <67125032+LucasPolatto@users.noreply.github.com>
2022-11-24 11:15:52 -0300

    Book e Loan POST / Alterar senha

--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -1,14 +1,19 @@
 public class LoginService
 {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string? User { get; set; }
     public string? Password { get; set; }
     public bool Logado { get; set; } = false;
 
     public void LogOut()
     {
-        Id = null;
+        Id = 0;
         User = null;
         Logado = false;
+    }
+
+    public int GetId()
+    {
+        return(Id);
     }
 }
